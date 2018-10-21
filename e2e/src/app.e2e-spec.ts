b9a566e73495edf23cdbc4a9809d273c75c6839c
Carlo THeunissen <carlotheunissen96@gmail.com>
2018-10-21 15:36:13 +0200

    e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,32 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display title', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Equation Solver');
   });
+
+
+  it('should display an input field', () => {
+    page.navigateTo();
+    expect(page.hasInput()).toBe(true);
+  });
+
+  function testError(input, result) {
+    it(input + ' should ' +(result ? '' : 'not ') + 'result in an error', function() {
+      page.submitText(input);
+      expect(page.hasError()).toBe(result);
+    });
+  }
+
+  const input = ["1*", "*1", "d", "\\as", "6*7v+"];
+  for(var x = 0; x < input.length; x++) {
+    testError(input[x], true);
+  }
+  const inputs = ["1*1", "d*1", "d+5", "a\\a", "6*7v"];
+  for(var x = 0; x < inputs.length; x++) {
+    testError(inputs[x], false);
+  }
+
+
 });
